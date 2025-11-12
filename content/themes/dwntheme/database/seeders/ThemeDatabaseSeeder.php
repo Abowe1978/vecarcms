6fd41e1dc0be30c9e5b36801007f23aed83f8f09
VeCar <148785074+Abowe1978@users.noreply.github.com>
2025-11-12 22:17:40 +0000

    commit

--- content/themes/dwntheme/database/seeders/ThemeDatabaseSeeder.php
+++ content/themes/dwntheme/database/seeders/ThemeDatabaseSeeder.php
@@ -4,17 +4,6 @@ namespace Themes\DwnTheme\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-require_once __DIR__ . '/ThemeSeeder.php';
-require_once __DIR__ . '/DWNThemeMediaSeeder.php';
-require_once __DIR__ . '/DWNThemeHomepageSeeder.php';
-require_once __DIR__ . '/DWNThemeAboutPageSeeder.php';
-require_once __DIR__ . '/DWNThemeContactPageSeeder.php';
-require_once __DIR__ . '/DWNThemeMenuSeeder.php';
-require_once __DIR__ . '/DWNThemeBlogSeeder.php';
-require_once __DIR__ . '/DWNThemeLegalPagesSeeder.php';
-require_once __DIR__ . '/DWNThemeSitemapSeeder.php';
-require_once __DIR__ . '/DWNThemeWidgetSeeder.php';
-
 class ThemeDatabaseSeeder extends Seeder
 {
     public function run(): void
